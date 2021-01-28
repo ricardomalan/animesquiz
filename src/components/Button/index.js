59d6c8a573219f3687d4bcf658000f489d214bbf
ricardomalan <ricardo.costa@bandtec.com.br>
2021-01-28 20:01:26 -0300

    Created the remaining components and updated the existing ones

---
+++ src/components/Button/index.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  border: 0;
+  width: 100%;
+  padding: 10px 16px;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 1;
+  text-transform: uppercase;
+  outline: 0;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    transition: .1s;
+    opacity: .6;
+  }
+  &:disabled {
+    background-color: #979797;
+    cursor: not-allowed;
+  }
+`;
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['submit', 'type', 'button']).isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Button;
